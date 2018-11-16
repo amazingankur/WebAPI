f973c99910f088ceb66db7a72662bc26fbcee5bf
amazingankur <anki1234@gmail.com>
2018-11-16 18:09:15 +1100

    Added files from local machine

---
+++ WebAPISolution/Controllers/ArrayCalculationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPISolution.Util;
+
+namespace WebAPISolution.Controllers
+{
+    public class ArrayCalculationController : ApiController
+    {
+        int[] ArrayOfIntegers = { 1, 2, 3, 4, 5 };
+        [Route("api/arraycalc/reverse")]
+        [HttpGet]
+        public IEnumerable<int> GetReverse()
+        {
+            for (int i = 0; i < ArrayOfIntegers.Length / 2; i++)
+            {
+                int temp = ArrayOfIntegers[i];
+                ArrayOfIntegers[i] = ArrayOfIntegers[ArrayOfIntegers.Length - i - 1];
+                ArrayOfIntegers[ArrayOfIntegers.Length - i - 1] = temp;
+            }
+            return ArrayOfIntegers;
+        }
+
+        [Route("api/arraycalc/deletepart")]
+        [HttpDelete]
+        public IEnumerable<int> Delete(int position)
+        {
+            return ArrayOfIntegers.RemoveAtIndex(position);
+        } 
+
+    }
+}
---
+++ WebAPISolution/Util/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPISolution.Util
+{
+    public static class ExtensionMethods
+    {
+        public static T[] RemoveAtIndex<T>(this T[] source, int index)
+        {
+            T[] destination = new T[source.Length - 1];
+            if (index > 0)
+                Array.Copy(source, 0, destination, 0, index);
+
+            if (index < source.Length - 1)
+                Array.Copy(source, index + 1, destination, index, source.Length - index - 1);
+
+            return destination;
+        }
+    }
+}
